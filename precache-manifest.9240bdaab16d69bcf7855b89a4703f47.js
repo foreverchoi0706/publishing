23846adef0028f54ec90b19cfca455bd561a4a55
foreverchoi0706 <57241273+foreverchoi0706@users.noreply.github.com>
2020-09-20 01:37:19 +0900

    Updates

---
+++ precache-manifest.9240bdaab16d69bcf7855b89a4703f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbb24de6e7143c6799e1f91caa97c13",
+    "url": "/sass_clone/index.html"
+  },
+  {
+    "revision": "6211383f4f140901ade4",
+    "url": "/sass_clone/static/css/main.e208d214.chunk.css"
+  },
+  {
+    "revision": "288cedd2d29f404469b3",
+    "url": "/sass_clone/static/js/2.24fb61ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sass_clone/static/js/2.24fb61ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6211383f4f140901ade4",
+    "url": "/sass_clone/static/js/main.44d6fbf4.chunk.js"
+  },
+  {
+    "revision": "f032fb86244fb450cda0",
+    "url": "/sass_clone/static/js/runtime-main.171cb7eb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sass_clone/precache-manifest.33c23ce4fdb40da3cbd49ae4f860bcc0.js"
+  "/sass_clone/precache-manifest.9240bdaab16d69bcf7855b89a4703f47.js"
 );
 
 self.addEventListener('message', (event) => {

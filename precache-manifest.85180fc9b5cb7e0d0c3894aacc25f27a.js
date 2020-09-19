800236f1e383b1906cf10a4a42ef45cfbc508489
foreverchoi0706 <57241273+foreverchoi0706@users.noreply.github.com>
2020-09-19 21:02:58 +0900

    Updates

---
+++ precache-manifest.85180fc9b5cb7e0d0c3894aacc25f27a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a561e40fe4c9991ed13352b9ad2e77e",
+    "url": "/sass_clone/index.html"
+  },
+  {
+    "revision": "332ff171a965435a6d1b",
+    "url": "/sass_clone/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "c83651267acd56a1fc79",
+    "url": "/sass_clone/static/js/2.5f62ad6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sass_clone/static/js/2.5f62ad6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "332ff171a965435a6d1b",
+    "url": "/sass_clone/static/js/main.c414c342.chunk.js"
+  },
+  {
+    "revision": "f032fb86244fb450cda0",
+    "url": "/sass_clone/static/js/runtime-main.171cb7eb.js"
+  }
+]);
